6bb4cda1708b1f63b6ca650949738fc27a46ed36
suellenvenancio <suellen.venancio@hotmail.com>
2026-01-12 14:49:29 -0300

    refactor: use util to upload image, change route of create user to sync creation with firebase

--- apps/doramar-api/src/services/user.service.ts
+++ apps/doramar-api/src/services/user.service.ts
@@ -1,11 +1,9 @@
-import actorsRepository from "../repository/actors.repository"
-import tvShowRepository from "../repository/tvshow.repository"
 import userRepository from "../repository/user.repository"
 import { RegisterUserInput, User } from "../types"
 import { AppError } from "../utils/errors"
 import { getAuth } from "firebase-admin/auth"
 
-import { put } from "@vercel/blob"
+import { uploadImage } from "../utils/upload-image-vercel"
 
 async function findUserByEmail(email: string) {
   const foundedUser = await userRepository.findByEmail(email)
@@ -14,21 +12,12 @@ async function findUserByEmail(email: string) {
     throw new AppError("User not found!", 404)
   }
 
-  const favoriteTvShow = await tvShowRepository.findFavoriteTvShowByUserId(
-    foundedUser?.id,
-  )
-  const favoriteActors = await actorsRepository.findFavoriteActorsByUserId(
-    foundedUser?.id,
-  )
-
   return {
     id: foundedUser?.id,
     username: foundedUser?.username,
     name: foundedUser?.name,
     email: foundedUser?.email,
     profilePicture: foundedUser?.profilePicture,
-    favoriteActors: favoriteActors.map((fav) => fav.actor) || [],
-    favoriteTvShow: favoriteTvShow?.tvShow,
     createdAt: foundedUser?.createdAt,
     updatedAt: foundedUser?.updatedAt,
   }
@@ -36,8 +25,6 @@ async function findUserByEmail(email: string) {
 
 async function findUserById(id: string) {
   const foundedUser = await userRepository.findUserById(id)
-  const favoriteTvShow = await tvShowRepository.findFavoriteTvShowByUserId(id)
-  const favoriteActors = await actorsRepository.findFavoriteActorsByUserId(id)
 
   return {
     id,
@@ -45,8 +32,6 @@ async function findUserById(id: string) {
     name: foundedUser.name,
     email: foundedUser.email,
     profilePicture: foundedUser.profilePicture,
-    favoriteActors: favoriteActors?.map((fav) => fav.actor) || [],
-    favoriteTvShow: favoriteTvShow?.tvShow,
     createdAt: foundedUser.createdAt,
     updatedAt: foundedUser.updatedAt,
   }
@@ -60,7 +45,8 @@ async function createUser(data: RegisterUserInput) {
         password: data.password,
         displayName: data.name,
       })
-      .then(async () => {
+      .then(async (user) => {
+        user.uid
         return await validateAndCreateUser(data)
       })
       .catch(async (error) => {
@@ -83,17 +69,25 @@ async function validateAndCreateUser({
   username,
   name,
   password,
+  id,
 }: {
   email: string
   username: string
   name: string
   password: string
+  id: string
 }) {
   try {
     await userRepository.validationUniqueEmail(email)
     await userRepository.validationUniqueUsername(username)
 
-    return await userRepository.createUser({ email, username, name, password })
+    return await userRepository.createUser({
+      email,
+      username,
+      name,
+      password,
+      id,
+    })
   } catch (error) {
     console.error("Error creating user:", error)
     throw error
@@ -140,80 +134,14 @@ async function uploadProfilePicture(userId: string, file: Express.Multer.File) {
     if (!user) {
       throw new AppError("User not found!", 404)
     }
-    const token = process.env.BLOB_VERCEL_READ_WRITE_TOKEN
 
-    const blob = await put(
-      `images/${Date.now()}-${file.originalname}`,
-      file.buffer,
-      {
-        access: "public",
-        token,
-      },
-    )
+    const blob = await uploadImage(file)
 
     return await userRepository.updateUser(userId, {
       profilePicture: blob.url,
     })
   } catch (error) {
     console.error("Error uploading avatar:", error)
-    throw error
-  }
-}
-
-async function addTVShowToFavorites(userId: string, tvShowId: string) {
-  try {
-    const user = await userRepository.findUserById(userId)
-
-    if (!user) {
-      throw new AppError("User not found!", 404)
-    }
-
-    const tvShow = await tvShowRepository.findTvShowById(tvShowId)
-
-    if (!tvShow) {
-      throw new AppError("TV Show not found!", 404)
-    }
-
-    const existentFavoriteTvShow =
-      await tvShowRepository.findFavoriteTvShowByUserId(userId)
-
-    if (
-      existentFavoriteTvShow &&
-      existentFavoriteTvShow?.tvShow.id !== tvShowId
-    ) {
-      const favoriteTvShow = await tvShowRepository.updateFavoriteTvShow(
-        userId,
-        tvShowId,
-        existentFavoriteTvShow.id,
-      )
-      return {
-        ...user,
-        favoriteTvShow: favoriteTvShow.tvShow,
-      }
-    }
-
-    if (
-      existentFavoriteTvShow &&
-      existentFavoriteTvShow?.tvShowId === tvShowId
-    ) {
-      await tvShowRepository.removeTvShowAsFavorite(existentFavoriteTvShow.id)
-      return {
-        ...user,
-        favoriteTvShow: undefined,
-      }
-    }
-
-    const favoriteTvShow = await tvShowRepository.addTVShowToFavorite(
-      userId,
-      tvShowId,
-    )
-
-    return {
-      ...user,
-      favoriteTvShow: favoriteTvShow.tvShow,
-    }
-  } catch (error) {
-    console.error("Error adding TV show to favorites:", error)
     throw error
   }
 }
@@ -234,7 +162,6 @@ const userService = {
   deleteUser,
   updateUser,
   findUserById,
-  addTVShowToFavorites,
   findUserByUsername,
 }
 
